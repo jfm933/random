0d117f366509ab258000735b94798c8e87d3f25e
James Fm <fmj2933@gmail.com>
2023-12-05 11:41:13 +0700

    Change Lib

--- lib/main.dart
+++ lib/main.dart
@@ -121,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => ReadData()));
               },
-              child: Text("Read Data"))
+              child: Text("Read User"))
         ],
       ),
     );
